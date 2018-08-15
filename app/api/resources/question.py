7b76df67cbf12a9d7e43823722113eefc5ef7fb5
Lazarus <lazarusodhis@gmail.com>
2018-08-15 13:56:08 +0300

    Add functionality for posting a question [Finishes #159791552]

--- app/api/resources/question.py
+++ app/api/resources/question.py
@@ -1,8 +1,33 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+from models.question import QuestionModel
 
 class Question(Resource):
     pass
 
 
 class QuestionList(Resource):
-    pass
+    parser = reqparse.RequestParser()
+    parser.add_argument('title', 
+        type = str,
+        required = True,
+        help = 'The title field is required.'
+    )
+    
+    parser.add_argument('description',
+        type = str,
+        required = True,
+        help = 'The description field is required.'
+    )
+
+    """Process the POST request for adding a question
+    """
+    @classmethod
+    def post(cls):
+        data = cls.parser.parse_args()
+
+        #Create a question object and pass the arguments
+        question = QuestionModel(data["title"], data["description"])
+
+        response = question.save()
+
+        return response, 201
---
+++ app/models/question.py
@@ -0,0 +1,39 @@
+#Declare the data structure to store our questions and answers (List)
+questions = [
+    {
+        "id": 1,
+        "title": "The default sample title",
+        "description": "The default sample QUESTION",
+        "answers": [
+            {
+                "id": 1,
+                "answer": "The default sample ANSWER"
+            }
+        ]
+    }
+]
+
+class QuestionModel:
+
+    """Initialise the Model class with the _id being
+                an optional parameter            
+        used _id since id is a Python keyword    
+    """
+    def __init__(self, title, description, _id=None):
+        self.title = title
+        self.description = description
+        self.id = _id
+
+    """Save the question to the questions list
+    """
+    def save(self):
+        #Create a new question dictionary
+        new_question = {
+            "id": questions[-1]['id'] + 1,
+            "title": self.title,
+            "description": self.description,
+            "answers": []
+        }
+        #append the dictionsry to the questions list
+        questions.append(new_question)
+        return new_question
