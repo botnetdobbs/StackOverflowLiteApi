80c3abdfaba68646c30e2df7893967dad162a122
Lazarus <lazarusodhis@gmail.com>
2018-08-18 19:57:36 +0300

    Add template for the home page [Finishes #159859023]

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api
 
 from api.resources.question import Question, QuestionList
@@ -18,44 +18,7 @@ api.add_resource(Answer, '/api/v1/questions/<int:questionID>/answers/<int:answer
 
 @app.route('/')
 def home():
-    return """Testing only available through Postman.\n\n
-    Add the following at the end of the Url:- 'api/v1/questions'...,\n
-    1. Posting a question\n
-    - POST _https://zstackoverflowliteapi.herokuapp.com/api/v1/question_\n\n
-
-    2. Fetch all questions\n
-    - GET _https://zstackoverflowliteapi.herokuapp.com/api/v1/question_\n\n
-
-    3. Fetch a specific question\n
-    - GET _https://zstackoverflowliteapi.herokuapp.com/api/v1/question/<int:questionID>_\n\n
-
-    4. Edit a specific question\n
-    - PUT _https://zstackoverflowliteapi.herokuapp.com/api/v1/question/<int:questionID>_\n\n
-
-    5. Delete a specific question\n
-    - DELETE _https://zstackoverflowliteapi.herokuapp.com/api/v1/question/<int:questionID>_\n\n
-
-    6. Posting an answer to the specific question\n
-    - POST _https://zstackoverflowliteapi.herokuapp.com/api/v1/question/<int:questionID>/answers_\n\n
-
-    7. Fetch an answer(s) to the specific question\n
-    - GET _https://zstackoverflowliteapi.herokuapp.com/api/v1/question/<int:questionID>/answers_\n\n
-
-    8. Fetch a specific answer from a specific question\n
-    - GET _https://zstackoverflowliteapi.herokuapp.com/api/v1/question/<int:questionID>/answers<<int:answerID>>_\n\n
-
-    9. Edit a specific answer from a specific question\n
-    - PUT _https://zstackoverflowliteapi.herokuapp.com/api/v1/question/<int:questionID>/answers<<int:answerID>>_\n\n
-
-    10. Delete a specific answer from a specific question\n
-    - DELETE _https://zstackoverflowliteapi.herokuapp.com/api/v1/question/<int:questionID>/answers<<int:answerID>>_\n
-
-    NB:-Unique Identifiers\n
-    > <int:questionID>\n
-    > <int:answerID>\n\n
-    
-    NB:- You can save yourself the burden of typing the long url using Postman variables\n
-    http://blog.getpostman.com/2014/02/20/using-variables-inside-postman-and-collection-runner/"""
+    return render_template('index.html')
 
 
 """To prevent the app from running when the file or something in the file is imported
