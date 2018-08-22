3748d8d63a522f3dec63d957116a01a7afb9d538
Lazarus <lazarusodhis@gmail.com>
2018-08-22 23:50:33 +0300

    Add database functionality to the QuestionModel class [Finishes #159962423]

--- api/models/question.py
+++ api/models/question.py
@@ -1,3 +1,5 @@
+from api.db import connect
+
 #Declare the data structure to store our questions and answers (List)
 questions = []
 
@@ -21,72 +23,74 @@ class QuestionModel:
     """
     @classmethod
     def find_by_id(cls, questionID):
-        for question in questions:
-            if question['id'] == questionID:
-                return cls(question['title'], question['description'], questionID)
-        return None
+        with connect() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM questions WHERE id = %s", (questionID,))
+                question = cursor.fetchone()
+                if question:
+                        return cls(question[2], question[3], questionID)
+                else:
+                    return None
 
     """Check if a question with the exact description 
-                exists in the list
+                exists in the questions table
     """
     @classmethod
     def find_by_description(cls, description):
-        for question in questions:
-            if question['description'] == description:
-                return True
-        return False
+        with connect() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM questions WHERE description = %s", (description,))
+                question = cursor.fetchone()
+                if question:
+                    return True
+                else:
+                    return None
 
+    """Get all questions from the questions table
+    """
     @classmethod
     def all(cls):
-        if questions:
-            return {"questions": questions}
-        return None
+        with connect() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM questions")
+                questions = cursor.fetchall()
+                #If questions is not None, loop through the questions
+                #append them in the quest list as dictionaries
+                if questions:
+                    #Create an empty list for storing the questions
+                    quest = []
+                    for q in questions:
+                        quest.append({"id": q[0], 
+                                        "user_id":q[1], 
+                                        "title": q[2], 
+                                        "description": q[3]})
+                    return {"questions": quest}
+                else:
+                    return None
             
 
-    """Save the question to the questions list
+    """Save the question to the questions table
     """
-    def save(self):
-        qst_id = 1
-        #Check if a questions are available
-        if len(questions) > 0:
-            #Add id(auto increment to the question id)
-            qst_id = questions[-1]["id"] + 1
-        #Create a new question dictionary
-        new_question = {
-            "id": qst_id,
-            "title": self.title,
-            "description": self.description,
-            "answers": []
-        }
-        #append the dictionsry to the questions list
-        questions.append(new_question)
-        return new_question
+    def save(self, user_id):
+        with connect() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute("INSERT INTO questions(user_id, title, description) VALUES(%s, %s, %s)", (user_id, self.title, self.description))
+                return {"message": "Question created successfully."}  
 
-    """Update the question in the questions list
+    """Update the question in the questions table
     """
     def update(self):
-        #Create a new dictionsry
-        updated_question = {
-            "title": self.title,
-            "description": self.description
-        }
-        #Check if the question dictionary exists in the questions list
-        #if it matches the object id, update and return True
+        #update the object and return True
         #else, return False
-        for question in questions:
-            if question['id'] == self.id:
-                question.update(updated_question)
+        with connect() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute("UPDATE questions SET title=%s, description=%s WHERE id=%s", (self.title, self.description, self.id))
                 return True
-        return False
 
-    """Delete the question in the questions list
+    """Delete the question in the questions table
     """
     def delete(self):
-        #Called global variable question
-        global questions
-        for question in questions:
-            if question['id'] == self.id:
-                #Filter through the list only retaining non natching questions
-                questions = list(filter(lambda x: x['id'] != self.id, questions))
+        with connect() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute("DELETE FROM questions WHERE id=%s", (self.id,))
                 return True
-        return False
