317d3d22bfaa08160d720e9d4d9d6a176e7dcc8b
Lazarus <lazarusodhis@gmail.com>
2018-08-22 14:06:41 +0300

    Override JWT default configs [Finishes #159923940]

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_restful import Api
 
 from api.resources.question import Question, QuestionList
 from api.resources.answer import Answer, AnswerList
+
+from datetime import timedelta
 
 from api.resources.user import UserRegister
 
@@ -14,8 +16,12 @@ from flask_jwt import JWT
 app = Flask(__name__)
 app.secret_key = 'xbt3ybot9'
 api = Api(app)
+#Custom authentification endpoint
+app.config['JWT_AUTH_URL_RULE'] = '/api/v1/auth/login'
+#Custom jwt timeout token to expire after an hour
+app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=3600)
 
-jwt = JWT(app, authenticate, identity) # Createa a new endpoint /auth
+jwt = JWT(app, authenticate, identity) # Createa a new endpoint (/auth) default. Overriden above
 #Register the resources
 api.add_resource(QuestionList, '/api/v1/questions')
 api.add_resource(Question, '/api/v1/questions/<int:questionID>')
@@ -23,7 +29,7 @@ api.add_resource(Question, '/api/v1/questions/<int:questionID>')
 api.add_resource(AnswerList, '/api/v1/questions/<int:questionID>/answers')
 api.add_resource(Answer, '/api/v1/questions/<int:questionID>/answers/<int:answerID>')
 
-api.add_resource(UserRegister, '/register')
+api.add_resource(UserRegister, '/api/v1/auth/register')
 
 @app.route('/')
 def home():
