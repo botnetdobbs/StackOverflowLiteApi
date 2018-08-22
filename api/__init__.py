e68aabd3cebcf0c5f98057314b9334f9a317e5b2
Lazarus <lazarusodhis@gmail.com>
2018-08-22 19:32:48 +0300

    Remove password hashing, temporary [Fixes #159923940]

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, jsonify
 from flask_restful import Api
-#Import the Bcrypr class for password hashing
-from flask_bcrypt import Bcrypt
 from datetime import timedelta
 from api.resources.question import Question, QuestionList
 from api.resources.answer import Answer, AnswerList
@@ -13,8 +11,7 @@ from flask_jwt import JWT
 
 
 app = Flask(__name__)
-#pass the flask app object to the Bcrypt class and store in bcrypt
-bcrypt = Bcrypt(app)
+
 app.secret_key = 'xbt3ybot9'
 api = Api(app)
 #Custom authentification endpoint
--- api/verify.py
+++ api/verify.py
@@ -1,10 +1,9 @@
 from api.models.user import UserModel as User
-from api import bcrypt
 
 #Referenced on Flask_JWT on https://pythonhosted.org/Flask-JWT/
 def authenticate(username, password):
     user = User.get_by_username(username)
-    if bcrypt.check_password_hash(user.password, password):
+    if user.password == password:
         return user
 
 def identity(payload):
