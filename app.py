6d639d1055d9db25e16b5aaca8badfc62afeb4ea
Lazarus <lazarusodhis@gmail.com>
2018-08-29 17:51:59 +0300

    Application runner fixed port [Fixes]

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ i.e if the file is run, the flask app will start,
 but if another file that imports app runs then it will not start
 """
 if __name__ == '__main__':
-    port = os.environ.get('PORT', 5000)
-    app.run(port)
+    # port = os.environ.get('PORT', 5000)
+    app.run(port = 5000)
