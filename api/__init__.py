3715935aded3f019b12901417f59cbc2bbc2cbb9
Lazarus <lazarusodhis@gmail.com>
2018-09-14 21:13:53 +0300

    Fix cross origin issues, extend token expity time and customize auth response [Fixes #160523508]

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, make_response
 from flask_restful import Api
 from datetime import timedelta
 from api.resources.question import Question, QuestionList
@@ -7,25 +7,36 @@ from api.resources.user import UserRegister, UserList
 
 from api.verify import authenticate, identity
 from api.create_tables import create_tables
+from flask_cors import CORS
 
 from flask_jwt import JWT
 
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.url_map.strict_slashes = False
-app.config.from_object('config.ProductionConfig')
+app.config.from_object('config.DevelopmentConfig')
 create_tables()
-api = Api(app) 
+api = Api(app)
 
 #Custom authentification endpoint
 app.config['JWT_AUTH_URL_RULE'] = '/api/v2/auth/login'
 
 #Get message when user ain't logged in 
 app.config['PROPAGATE_EXCEPTIONS'] = True
-#Custom jwt timeout token to expire after an hour
-app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=3600)
+#Custom jwt timeout token to expire after an 12 hour2
+app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=43200)
 
 jwt = JWT(app, authenticate, identity) # Createa a new endpoint (/auth) default. Overriden above
+#Customize the Auth response handler to include the user_id object param
+@jwt.auth_response_handler
+def customized_response_handler(access_token, identity):
+    return jsonify({
+                        'access_token': access_token.decode('utf-8'),
+                        'user_id': identity.id,
+                        'username': identity.username,
+                        'email': identity.email
+                   })
 #Register the resources
 api.add_resource(QuestionList, '/api/v2/questions')
 api.add_resource(Question, '/api/v2/questions/<int:questionID>')
